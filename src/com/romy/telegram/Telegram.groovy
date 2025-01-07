0e126c233623a27962e445bf7ae72d5409baa92b
romy <romysaputrasihanandaa@gmail.com>
2025-01-08 01:57:51 +0700

    update 'class Telegram'

--- src/com/romy/telegram/Telegram.groovy
+++ src/com/romy/telegram/Telegram.groovy
@@ -4,15 +4,17 @@ class Telegram {
     String botToken
     String chatId
     String messageThreadId
+    def ctx
 
-    Telegram(String botToken, String chatId, String messageThreadId) {
+    Telegram(String botToken, String chatId, String messageThreadId, def ctx) {
         this.botToken = botToken
         this.chatId = chatId
         this.messageThreadId = messageThreadId
+        this.ctx = ctx
     }
 
     def sendMessage(String message, String parseMode = "MarkDown") {
-        sh """
+        this.ctx.sh """
         curl -X POST https://api.telegram.org/bot${this.botToken}/sendMessage \
         -H "Content-Type: application/json" \
         -d '{"chat_id": "${this.chatId}", "message_thread_id": "${this.messageThreadId}", "text": "${message}", "disable_notification": true, "parse_mode":"${parseMode}"}'
