5ceefb48e2e59b95cb80f7e3425dd491d7a048fb
romy <romysaputrasihanandaa@gmail.com>
2025-01-07 12:41:55 +0700

    first commit

---
+++ src/com/romy/telegram/Telegram.groovy
@@ -0,0 +1,21 @@
+package com.romy.telegram
+
+class Telegram {
+    String botToken
+    String chatId
+    String messageThreadId
+
+    TelegramHelper(String botToken, String chatId, String messageThreadId) {
+        this.botToken = botToken
+        this.chatId = chatId
+        this.messageThreadId = messageThreadId
+    }
+
+    def sendMessage(String message, String parseMode = "MarkDown") {
+        sh """
+        curl -X POST https://api.telegram.org/bot${this.botToken}/sendMessage \
+        -H "Content-Type: application/json" \
+        -d '{"chat_id": "${this.chatId}", "message_thread_id": "${this.messageThreadId}", "text": "${message}", "disable_notification": true, "parse_mode":"${parseMode}"}'
+        """
+    }
+}
